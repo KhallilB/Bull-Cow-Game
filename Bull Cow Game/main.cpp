146c47e99eea701150d2836efb7dd519f741d2ec
KhallilB <khallil.bailey@gmail.com>
2019-01-24 22:38:55 -0800

    first commit

---
+++ Bull Cow Game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bull Cow Game
+//
+//  Created by Khallil Bailey on 1/24/19.
+//  Copyright © 2019 Khallil Bailey. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
