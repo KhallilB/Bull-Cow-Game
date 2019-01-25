8727082ec3e0e7d1a08edcac9fe7f9f00b0d4ab2
KhallilB <khallil.bailey@gmail.com>
2019-01-25 01:25:50 -0800

    added a loop for player turns

--- Bull Cow Game/main.cpp
+++ Bull Cow Game/main.cpp
@@ -11,25 +11,47 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrint();
+
+//Start of Application
 int main() {
+    
+    PrintIntro();
+    
+    //Loop for guesses player gets
+    constexpr int NUMBER_OF_TURNS = 5;
+    for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+        GetGuessAndPrint();
+    }
+    
+    
+
+    cout << endl;
+    return 0;
+}
+
+void PrintIntro() {
     
     constexpr int WORD_LENGTH = 5;
     
     //Introduces the game
-    ::cout << "Welcome To Bulls and Cows a fun word game.\n";
+    ::cout << "Welcome To Bulls and Cows, a fun word game.\n";
     ::cout << "Can You guess the " << WORD_LENGTH;
     ::cout << " letter isogram I'm thinking of?\n";
     ::cout << endl;
-    
+}
+
+string GetGuessAndPrint() {
     //Get Guess
     ::cout << "Enter Your Guess: ";
     ::string Guess = "";
     getline(cin, Guess);
     
-    //Rpeat back to them
-    cout << "Your guess was: " << Guess;
+    //Prints guess
+    ::cout << "Your guess was: " << Guess;
+    ::cout << endl;
+    ::cout << endl;
+    return Guess;
     
-    
-    cout << endl;
-    return 0;
 }
